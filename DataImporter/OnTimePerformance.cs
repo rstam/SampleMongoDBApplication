30944a54567cdcf34acdfb4e52459accb1b6373b
rstam <robert@robertstam.org>
2015-05-10 17:46:07 -0400

    Initial version of DataImporter.

---
+++ DataImporter/OnTimePerformance.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataImporter
+{
+    public class OnTimePerformance
+    {
+        public int? YEAR { get; set; }
+        public int? QUARTER { get; set; }
+        public int? DAY_OF_MONTH { get; set; }
+        public int? DAY_OF_WEEK { get; set; }
+        public DateTime? FL_DATE { get; set; }
+        public string UNIQUE_CARRIER { get; set; }
+        public int? AIRLINE_ID { get; set; }
+        public string CARRIER { get; set; }
+        public int? FL_NUM { get; set; }
+        public int? ORIGIN_AIRPORT_ID { get; set; }
+        public int? ORIGIN_AIRPORT_SEQ_ID { get; set; }
+        public int? ORIGIN_CITY_MARKET_ID { get; set; }
+        public string ORIGIN { get; set; }
+        public string ORIGIN_CITY_NAME { get; set; }
+        public string ORIGIN_STATE_ABR { get; set; }
+        public int? DEST_AIRPORT_ID { get; set; }
+        public int? DEST_AIRPORT_SEQ_ID { get; set; }
+        public int? DEST_CITY_MARKET_ID { get; set; }
+        public string DEST { get; set; }
+        public string DEST_CITY_NAME { get; set; }
+        public string DEST_STATE_ABR { get; set; }
+        public string CRS_DEP_TIME { get; set; }
+        public string DEP_TIME { get; set; }
+        public double? DEP_DELAY { get; set; }
+        public string CRS_ARR_TIME { get; set; }
+        public string ARR_TIME { get; set; }
+        public double? ARR_DELAY { get; set; }
+        public double? CANCELLED { get; set; }
+        public string CANCELLATION_CODE { get; set; }
+        public double? DIVERTED { get; set; }
+        public double? DISTANCE { get; set; }
+        public double? CARRIER_DELAY { get; set; }
+        public double? WEATHER_DELAY { get; set; }
+        public double? NAS_DELAY { get; set; }
+        public double? SECURITY_DELAY { get; set; }
+        public double? LATE_AIRCRAFT_DELAY { get; set; }
+    }
+}
---
+++ DataImporter/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CsvHelper;
+using MongoDB.Driver;
+
+namespace DataImporter
+{
+    public static class Program
+    {
+        private static IMongoClient __client;
+        private static IMongoDatabase __database;
+        private static IMongoCollection<OnTimePerformance> __collection;
+
+        public static void Main(string[] args)
+        {
+            try
+            {
+                MainAsync(args).GetAwaiter().GetResult();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception: {0}", ex);
+            }
+
+            Console.WriteLine("Press Enter");
+            Console.ReadLine();
+        }
+
+        private static async Task MainAsync(string[] args)
+        {
+            var connectionString = "mongodb://localhost";
+            __client = new MongoClient(connectionString);
+            __database = __client.GetDatabase("Flights");
+            __collection = __database.GetCollection<OnTimePerformance>("OnTimePerformance");
+
+            await LoadOnTimePerformanceDataAsync();
+        }
+
+        private static async Task LoadOnTimePerformanceDataAsync()
+        {
+            Console.WriteLine("Loading OnTimePerformance files");
+
+            await __database.DropCollectionAsync(__collection.CollectionNamespace.CollectionName);
+
+            var dataDirectory = FindDataDirectory();
+            var onTimePerformanceDataDirectory = Path.Combine(dataDirectory, "OnTimePerformance");
+
+            foreach (var csvFileName in Directory.GetFiles(onTimePerformanceDataDirectory, "*.csv"))
+            {
+                await LoadOnTimePerformanceDataAsync(csvFileName);
+            }
+        }
+
+        private static string FindDataDirectory()
+        {
+            var directory = Directory.GetCurrentDirectory();
+
+            var leaf = Path.GetFileName(directory);
+            if (leaf == "Debug" || leaf == "Release")
+            {
+                directory = Path.GetDirectoryName(directory);
+                directory = Path.GetDirectoryName(directory);
+            }
+
+            return Path.Combine(directory, "Data");
+        }
+
+        private static async Task LoadOnTimePerformanceDataAsync(string csvFilename)
+        {
+            Console.WriteLine("Loading {0}", csvFilename);
+
+            var batchSize = 1000;
+            using (var csvReader = new CsvReader(new StreamReader(csvFilename)))
+            {
+                var batch = new List<OnTimePerformance>();
+
+                foreach (var document  in csvReader.GetRecords<OnTimePerformance>())
+                {
+                    if (document.FL_DATE.HasValue)
+                    {
+                        document.FL_DATE = DateTime.SpecifyKind(document.FL_DATE.Value, DateTimeKind.Utc);
+                    }
+
+                    batch.Add(document);
+                    if (batch.Count == batchSize)
+                    {
+                        await __collection.InsertManyAsync(batch);
+                        batch.Clear();
+                        Console.Write(".");
+                    }
+                }
+
+                if (batch.Count > 0)
+                {
+                    await __collection.InsertManyAsync(batch);
+                }
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
